8d8f05f947465ef831b44688f30e88375157d92f
antoni909 <antoni.dev0@gmail.com>
2023-01-09 20:17:21 +0000

    learn basic shebang, using basic std in out with python

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""print() - display data to std out"""
+
+# defines the main fn
+def main():
+    user_input = input("please enter IPv4 address")
+    print("IPv4 address: " + user_input)
+
+# calls the main fn
+main()
