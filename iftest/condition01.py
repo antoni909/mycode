cce961cc292aed77199cd70f8488a6a35670592e
antoni909 <antoni.dev0@gmail.com>
2023-01-10 22:29:41 +0000

    create ifel dir and condition01.py, learn python if statements

---
+++ iftest/condition01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+hostname= input("what should be set for hostname?")
+if hostname == "MTG":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+print("Exiting the script")
