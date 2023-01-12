c4e8af866f4eb2c066ba302adab599a0a140e24a
antoni909 <antoni.dev0@gmail.com>
2023-01-12 22:31:03 +0000

    create oop-intro dir and dice game to learn classes in python

---
+++ oop-intro/first-oop.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""RZFeeser | Alta3 Research
+   Creating a simple dice program utilizing classes."""
+
+# standard library
+from random import randint
+
+
+class Player:
+    def __init__(self):
+        self.dice = []
+
+    def roll(self):
+        self.dice = []
+        for i in range(3):
+            self.dice.append(randint(1,6))
+    def get_dice(self): 
+        return self.dice
+def main():
+    """called at run time"""
+
+    ## create our player objects
+    player1 = Player()
+    player2 = Player()
+
+    # player objects can use their method "roll"
+    player1.roll()
+    player2.roll()
+
+    # the f-string is simply a "fill in the {} with the value inside"
+    print(f"Player 1 rolled {player1.get_dice()}")
+    print(f"Player 2 rolled {player2.get_dice()}")
+
+    # determine which player won
+    if sum(player1.get_dice()) == sum(player2.get_dice()):
+        print("Draw!")
+    elif sum(player1.get_dice()) > sum(player2.get_dice()):
+        print("Player 1 wins!")
+    else:
+        print("Player 2 wins!")
+
+
+if __name__ == "__main__":
+    main()
